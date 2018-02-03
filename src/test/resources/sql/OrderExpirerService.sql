30b37e701dd8aca74d2f343d2cd668ba15cad2ec
htchepannou <htchepannou@expedia.com>
2018-02-03 14:18:38 -0500

    fix test

--- src/test/resources/sql/OrderExpirerService.sql
+++ src/test/resources/sql/OrderExpirerService.sql
@@ -29,5 +29,5 @@ INSERT INTO T_ORDER_LINE(id, order_fk, merchant_id, booking_id, offer_type, offe
 INSERT INTO T_ORDER(id, site_id, customer_id, expiry_datetime, status, total_amount, currency_code, payment_method, payment_id, first_name, last_name, email)
   VALUES(200, 1, 3, '2030-10-11', 'CONFIRMED', '6000', 'XAF', 'ONLINE', 123, 'Ray', 'Sponsible', 'ray@gmail.com');
 
-INSERT INTO T_ORDER_LINE(id, order_fk, merchant_id, booking_id, offer_type, offer_token, quantity, unit_price, total_price, description)
-  VALUES(200, 200, 200, 5678, 'CAR', '100,1902459600000,1902466800000,1000,6000,XAF,1,2370001,2370002,15138644923060', 1, 6000, 6000, 'hello');
+INSERT INTO T_ORDER_LINE(id, order_fk, merchant_id, booking_id, offer_type, offer_token, quantity, unit_price, total_price, description, fees, net_price)
+  VALUES(200, 200, 200, 5678, 'CAR', '100,1902459600000,1902466800000,1000,6000,XAF,1,2370001,2370002,15138644923060', 1, 6000, 6000, 'hello', 0, 0);

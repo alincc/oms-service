af2db58641189ba7c276164e9c5e9a678187b02b
htchepannou <htchepannou@expedia.com>
2018-02-16 05:05:23 -0500

    disable flyway in non-dev

--- src/main/java/io/tchepannou/enigma/oms/config/FlywayConfiguration.java
+++ src/main/java/io/tchepannou/enigma/oms/config/FlywayConfiguration.java
@@ -4,8 +4,10 @@ import org.flywaydb.core.Flyway;
 import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("dev")
 public class FlywayConfiguration {
     @Bean
     public FlywayMigrationStrategy flywayMigrationStrategy(){

896cb295d97c4d691bd60a6375f6a2e82e009093
htchepannou <htchepannou@expedia.com>
2018-03-24 15:19:10 -0400

    fix sender

--- src/main/java/io/tchepannou/enigma/oms/service/ticket/TicketSmsSender.java
+++ src/main/java/io/tchepannou/enigma/oms/service/ticket/TicketSmsSender.java
@@ -1,10 +1,8 @@
 package io.tchepannou.enigma.oms.service.ticket;
 
 import io.tchepannou.core.logger.KVLogger;
-import io.tchepannou.enigma.oms.backend.refdata.SiteBackend;
 import io.tchepannou.enigma.oms.domain.Ticket;
 import io.tchepannou.enigma.oms.service.sms.SmsGateway;
-import io.tchepannou.enigma.refdata.client.dto.SiteDto;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,25 +19,18 @@ public class TicketSmsSender {
     private TicketSmsGenerator messageGenerator;
 
     @Autowired
-    private SiteBackend siteBackend;
-
-    @Autowired
     private SmsGateway gateway;
 
     public String send(final Ticket ticket) {
-        final SiteDto site = siteBackend.findById(ticket.getOrderLine().getOrder().getSiteId());
-
-        final String sender = site.getBrandName();
         final String mobileNumber = ticket.getOrderLine().getOrder().getMobileNumber();
         final String message = messageGenerator.generate(ticket);
-        logger.add("SmsSender", sender);
         logger.add("SmsNumber", mobileNumber);
         logger.add("SmsMessage", message);
         logger.add("SmsGateway", gateway.getClass());
 
         try {
 
-            final String result = gateway.send(sender, mobileNumber, message);
+            final String result = gateway.send(null, mobileNumber, message);
             logger.add("SmsTransactionID", result);
             return result;
 
--- src/test/java/io/tchepannou/enigma/oms/service/ticket/TicketSmsSenderTest.java
+++ src/test/java/io/tchepannou/enigma/oms/service/ticket/TicketSmsSenderTest.java
@@ -1,12 +1,10 @@
 package io.tchepannou.enigma.oms.service.ticket;
 
 import io.tchepannou.core.logger.KVLogger;
-import io.tchepannou.enigma.oms.backend.refdata.SiteBackend;
 import io.tchepannou.enigma.oms.domain.Order;
 import io.tchepannou.enigma.oms.domain.OrderLine;
 import io.tchepannou.enigma.oms.domain.Ticket;
 import io.tchepannou.enigma.oms.service.sms.SmsGateway;
-import io.tchepannou.enigma.refdata.client.dto.SiteDto;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -29,18 +27,12 @@ public class TicketSmsSenderTest {
     @Mock
     private SmsGateway gateway;
 
-    @Mock
-    private SiteBackend siteBackend;
-
     @InjectMocks
     private TicketSmsSender service;
 
     @Test
     public void shouldSendSms() throws Exception {
         // Given
-        final SiteDto site = createSite(1, "Test");
-        when(siteBackend.findById(1)).thenReturn(site);
-
         final Ticket ticket = createTicket("5147550101", 1);
         when(generator.generate(ticket)).thenReturn("This is a message");
 
@@ -51,15 +43,12 @@ public class TicketSmsSenderTest {
 
         // Then
         assertThat(result).isEqualTo("123");
-        verify(gateway).send(site.getBrandName(),"5147550101", "This is a message");
+        verify(gateway).send(null,"5147550101", "This is a message");
     }
 
     @Test
     public void shouldLogMessage() throws Exception {
         // Given
-        final SiteDto site = createSite(1, "Test");
-        when(siteBackend.findById(1)).thenReturn(site);
-
         final Ticket ticket = createTicket("5147550101", 1);
         when(generator.generate(ticket)).thenReturn("This is a message");
         when(gateway.send(any(), any(), any())).thenReturn("12345");
@@ -68,17 +57,9 @@ public class TicketSmsSenderTest {
         service.send(ticket);
 
         // When
-        verify(logger).add("SmsSender", "Test");
         verify(logger).add("SmsNumber", "5147550101");
         verify(logger).add("SmsMessage", "This is a message");
         verify(logger).add("SmsTransactionID", "12345");
-    }
-
-    private SiteDto createSite(final Integer id, final String name){
-        SiteDto site = new SiteDto();
-        site.setBrandName(name);
-        site.setId(id);
-        return site;
     }
 
     private Ticket createTicket(final String number, final Integer siteId){
